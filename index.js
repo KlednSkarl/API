ac45ed850635a5b858681e2bc2b8726a7d56c01a
Nicko Osabel <nickoosabel@gmail.com>
2025-06-24 19:28:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// server.js
+// Install this first before running the query npm install express mssql cors
+const express = require('express');
+const sql = require('mssql');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const config = {
+  user: 'SysDev',
+  password: 'Sys2023Dev@BMS',
+  server: '116.50.222.51',
+  port: 50130,
+  database: 'TCM_CSS',
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};
+
+app.get('/loans', async (req, res) => {
+  try {
+    const pool = await sql.connect(config);
+    const result = await pool.request().query('SELECT * FROM CS_tblLoan');
+    res.json(result.recordset);
+  } catch (err) {
+    res.status(500).send('Database Error');
+  } finally {
+    sql.close();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`✅ API running at http://localhost:${port}/loans`);
+});
